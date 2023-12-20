1ef0adb81bc39f678d55d21025f3870ff68e0896
Anusha <143824335+Anusha-Yadavv@users.noreply.github.com>
2023-12-20 14:01:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+
+let inputElement=document.getElementById("fname");
+inputElement.addEventListner("mouseLeave",()=>{
+	let inputElementValue=inputElement.value;
+	inputElementValue.toUpperCase();
+})
